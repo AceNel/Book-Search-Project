b46a903b044f7248d7e0b7e10881405bc18924da
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-04 09:02:28 +0300

    Fix html links bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-
+//constants for the api key, the search button and the input field where the user types
+const api_key = "AIzaSyDdkUJrGPPVWMRg-enDhs2Y_2xvkvQT6lI";
+const search_button = document.getElementById("search-button");
+const search_input = document.getElementById("search-input");
+const displayArea = document.getElementById("result");
