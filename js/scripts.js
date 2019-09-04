737042352c7e19450eac2667c37a9893477dd186
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-04 09:06:05 +0300

    Add an event listener for search button

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ const api_key = "AIzaSyDdkUJrGPPVWMRg-enDhs2Y_2xvkvQT6lI";
 const search_button = document.getElementById("search-button");
 const search_input = document.getElementById("search-input");
 const displayArea = document.getElementById("result");
+
+//event listeners for the search button and the enter press
+search_button.addEventListener("click", search);
+document.addEventListener("keypress", function (e) {
+  if (event.keyCode == 13) {
+    search(e);
+  }
+})
